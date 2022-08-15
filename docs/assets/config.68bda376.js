6265bf1bb6696a16c13a979ab001a0266821383c
Valentin Tarasov <tarasowalentin@gmail.com>
2022-08-15 19:31:45 +0200

    docs changes

--- docs/assets/config.68bda376.js
+++
@@ -1,2 +0,0 @@
-import{d as t,r as m,a as s}from"./decorateStory.1dc18666.js";import"./iframe.4bbfe56c.js";import"./vue.esm-bundler.b7b75245.js";var a={framework:"vue3"};export{t as applyDecorators,a as parameters,m as render,s as renderToDOM};
-//# sourceMappingURL=config.68bda376.js.map
